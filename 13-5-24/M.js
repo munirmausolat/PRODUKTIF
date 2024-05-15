246262cf21d1623a39a8fc12e5f6fdb459f6f842
munirmausolat <munir43344@gmail.com>
2024-05-15 13:05:24 +0700

    Create M.js

---
+++ 13-5-24/M.js
@@ -0,0 +1,47 @@
+
+//let// 
+
+let x= 21 + 14
+ x= 10 + 11
+console.log(x)
+
+let a= 32 + 4
+ a-= 10 + 14
+console.log(a) 
+
+let p= 21 - 14
+ p*= 10 + 11
+console.log(p)
+
+let q= 2 + -15
+ q!= 7 + 11
+console.log(q)
+
+let r= 2 - 15
+ r/= 7 - 11
+console.log(r)
+
+//var//
+
+var y= 10 + 11
+var y= 11 + 10
+console.log(y)
+
+var y= 20 + 19
+ y+= 12 + 7
+console.log(y)
+
+var b= 34 + 11
+ b*= 11 / 10
+console.log(b)
+
+var y= 6 * 21
+var y= 7 - 1
+console.log(y)
+
+var w= 0 * 1
+ w= 9 + 10
+console.log(w)
+
+console.log(u)
+var u=15;
