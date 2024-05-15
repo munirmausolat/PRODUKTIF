42bceeece967e0c0dd15ede41d0694404a12baa5
munirmausolat <munir43344@gmail.com>
2024-05-15 13:08:25 +0700

    Create Mm.js

---
+++ 13-5-24/13-5-24/Mm.js
@@ -0,0 +1,29 @@
+
+var x="alex";
+    x+=2;
+    console.log(x)
+    
+var x="arnold";
+    x+=6;
+    console.log(x)
+    
+var x="rodrygo";
+    x+=10;
+    console.log(x)
+    
+var x="gabriel";
+    x+=14;
+    console.log(x)
+    
+var x="victoria";
+    x+=26;
+    console.log(x)
+    
+//id//
+
+var y={nama:"munir",umur:16,absen:17,kelas:"X RPL 3"};
+console.log(y.nama,y.umur,y.absen,y.kelas)
+console.log(y.nama)
+console.log(y.umur)
+console.log(y.absen)
+console.log(y.kelas)
